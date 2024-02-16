856adb038d214a2d723962af9a1da64d06857dec
roman hrytsiuk <rmngrck97@gmail.com>
2024-02-16 17:40:13 +0200

    corrected news

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -3,8 +3,17 @@ import News from '@/components/News.vue'
 import SideMenu from '@/components/SideMenu.vue'
 </script>
 <template>
-    <div class="flex gap-5 justify-content-between">
+    <div class="content flex gap-5 justify-content-between">
         <News />
         <SideMenu />
     </div>
 </template>
+
+<style scoped lang="scss">
+    .content {
+
+        @media (max-height: 800px) {
+            margin-top: 3rem;
+        }
+    }
+</style>
