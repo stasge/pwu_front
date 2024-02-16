5650b489e001baf58f30e80cbcfb6e647f934933
roman hrytsiuk <rmngrck97@gmail.com>
2024-02-16 11:58:18 +0200

    remove vue files

---
+++ src/views/MainView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+
+</script>
+
+<template>
+    <div class="wrapper">
+        <Header />
+        <div class="container">
+
+        </div>
+        <Footer />
+    </div>
+</template>
