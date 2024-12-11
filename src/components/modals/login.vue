3a47bada8c14c9cde5ca1fa2a49f56cd5953b1ba
roman hrytsiuk <rmngrck97@gmail.com>
2024-12-11 19:25:40 +0200

    build with code verification handler during login

--- src/components/modals/login.vue
+++ src/components/modals/login.vue
@@ -2,6 +2,7 @@
 import Modal from '@/components/base/modal.vue'
 import { useAsyncCallWrapper } from '@/composables/useAsyncCallWrapper';
 import { useUserStore } from '@/stores/userStore';
+import { fetchGet, fetchPost } from '@/utils/fetchApi';
 import useVuelidate from '@vuelidate/core';
 import { required } from '@vuelidate/validators';
 import { reactive, ref } from 'vue';
@@ -13,10 +14,21 @@ const {wrapAsyncCall} = useAsyncCallWrapper()
 const userStore = useUserStore()
 const showed = ref(false)
 const passwordHidden = ref(true)
+const needVerification = ref(false)
 const form = reactive({
     username: '',
     pass: ''
 })
+
+const verificationForm = reactive({
+    code: ''
+})
+
+const verificationRules = {
+    code: {required}
+}
+const verificationV$ = useVuelidate(verificationRules, verificationForm)
+
 const rules = {
     username: {required},
     pass: {required},
@@ -47,19 +59,50 @@ const login = async () => {
         if (e.status === 401) {
             toast.error("Невірний логін або пароль")
         }
+        if (e.status === 403) {
+            needVerification.value = true
+        }
         return true
     }, 'Ви успішно увішли до облікого запису')
+}
+
+const verify = async () => {
+    if (!await verificationV$.value.$validate()) {
+        return
+    }
+
+    await wrapAsyncCall(async () => {
+        await fetchPost('user/verification', {code: verificationForm.code})
+    }, 
+    (e) => {
+        if (e.status === 401) {
+            toast.error("Код верифікації застарів, натисніть 'Відправити код повторно'")
+        }
+        return true
+    }, 
+    'Ви успішно верифікували обліковий запис')
+}
+
+const sendVerificationCode = async () => {
+    await wrapAsyncCall(async () => {
+        await fetchGet('user/verification', {})
+    }, 
+    (e) => {
+        if (e.status === 401) {
+            toast.error("")
+        }
+        return true
+    }, 
+    'Код верифікації було відправлено повторно. Перевірте вашу електрону пошту')
 }
 
 defineExpose({showDia})
 </script>
 <template>
     <Modal v-model:showed="showed">
-        <template #header>
-            <h2 class="modal__title mb-5">Увійти</h2>
-        </template>
         <template #body>
-            <form @submit.prevent="login" class="flex flex-column justify-content-center">
+            <form v-if="!needVerification" @submit.prevent="login" class="flex flex-column justify-content-center">
+                <h2 class="modal__title mb-5">Увійти</h2>
                 <div class="field w-full">
                     <label for="login" class="w-full">Логін</label>
                     <input 
@@ -91,6 +134,21 @@ defineExpose({showDia})
                     <span @click="emit('openRegistration'), showed = false" class="underline cursor-pointer">Зареєструватися</span>
                 </div>
                 <button type="submit" class="btn btn-sm mt-3 align-self-center">Увійти</button>
+            </form>
+            <form @submit.prevent="verify" class="flex flex-column justify-content-center" v-else>
+                <h2 class="text-center mb-5">На вашу пошту було відправлено код для верифікації</h2>
+                <div class="field w-full">
+                    <label for="login" class="w-full">Код для верифікації</label>
+                    <input 
+                        v-model="verificationForm.code" 
+                        id="login" 
+                        type="text"
+                        :class="{invalid: v$.username.$error}" 
+                        class="text-base text-color p-2 surface-overlay border-1 border-solid appearance-none outline-none focus:border-primary w-full"
+                    >
+                </div>
+                <span @click="sendVerificationCode" class="underline cursor-pointer">Відправити код повторно</span>
+                <button type="submit" class="btn btn-sm mt-3 align-self-center">Відправити</button>
             </form>
         </template>
     </Modal>
