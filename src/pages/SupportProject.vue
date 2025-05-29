0653511993d550ecc9eaa332f01befe560d05e57
R-omek <rmngrck97@gmail.com>
2025-05-29 19:54:30 +0300

    change support method

--- src/pages/SupportProject.vue
+++ src/pages/SupportProject.vue
@@ -1,11 +1,23 @@
 <script setup lang="ts">
+import { useToast } from 'vue-toastification';
+
+const toast = useToast()
+
+
+const copyToClipboard = (text: string) => {
+    navigator.clipboard.writeText(text).then(() => {
+        toast.success('Cкопійовано в буфер обміну!');
+    }).catch(err => {
+        console.error('Помилка при копіюванні тексту:', err);
+    });
+};
 </script>
 <template>
     <div class="support-project">
         <div class="support-project__inner w-full">
             <div class="support-project__container">
                 <h1 class="support-project__title">Підтримати проєкт</h1>
-                <div class="support-project__text">
+                <div class="support-project__text text">
                     <p>Наша команда розвиває сервер із любов’ю до гри та гравців. Якщо ти бажаєш підтримати наш проєкт — ти можеш зробити добровільний внесок.</p>
                     <p>💛 Усі зібрані кошти використовуються на:</p>
                     <ul>
@@ -17,7 +29,45 @@
                     <p>✨ Як знак подяки за твою підтримку, ми нараховуємо внутрішньоігровий бонус:</p>
                     <p><b>1 золота монета за кожні 3 грн внеску.</b></p>
                     <p class="mt-2"><b>Мінімальна сума для нарахування бонусу — 10 грн (через обмеження банку).</b></p>
-                    <h3 class="my-5">Обовʼязково після переказу <a class="text-3xl" href="https://www.privat24.ua/send/hbi55" target="_blank" rel="noopener noreferrer">за цим посиланням</a>, <b> завантажте квитанцію та логін ІГРОВОГО акаунту (використовується для входу в гру) в приватні повідомлення discord адміністратору - Roman</b>, щоб отримати бонус.</h3>
+                    <h2>Реквізити для оплати</h2>
+                    <div class="text__requisites">
+                        <p class="flex flex-wrap">
+                            <span><b class="mr-3">Найменування отримувача: </b></span>
+                            <span>ФОП Гичак Ольга Володимирівна</span>
+                        </p>
+                        <p class="flex flex-wrap">
+                            <span><b class="mr-3">Код отримувача: </b></span>
+                            <span>
+                                3356210484
+                                <i
+                                    class="pi pi-copy text-xl cursor-pointer"
+                                    v-tooltip.top="'Копіювати'"
+                                    @click="copyToClipboard('3356210484')"
+                                ></i>
+                            </span>
+                        </p>
+                        <p class="flex flex-wrap">
+                            <span><b class="mr-3">Рахунок отримувача: </b></span>
+                            <span>
+                                UA153052990000026006024927081
+                                <i
+                                    @click="copyToClipboard('UA153052990000026006024927081')"
+                                    class="pi pi-copy text-xl cursor-pointer"
+                                    v-tooltip.top="'Копіювати'"
+                                ></i>
+                            </span>
+                        </p>
+                        <p class="flex flex-wrap">
+                            <span><b class="mr-3">Призначення платежу: </b></span>
+                            <span>"на розвиток серверу"</span>
+                        </p>
+                        <p class="flex flex-wrap">
+                            <span><b class="mr-3">Назва банку: </b></span>
+                            <span>АТ КБ "ПРИВАТБАНК"</span>
+                        </p>
+                    </div>
+                    <h3 class="my-2">УВАГА! Обовʼязково <b> завантажте квитанцію та логін ІГРОВОГО акаунту (використовується для входу в гру) в приватні повідомлення discord адміністратору - Roman(romanhrytsiuk)</b>, щоб отримати бонус.</h3>
+                    <img class="block" src="@/assets/images/qr-bank.png" alt="" width="300">
                     <p>Це не продаж ігрових цінностей, а вдячність за допомогу, яку ми щиро цінуємо 🙏</p>
                 </div>
             </div>
@@ -49,9 +99,11 @@
         margin-bottom: 24px;
         color: #fff;
     }
+    
     &__text {
         font-size: 1.1rem;
         line-height: 1.7;
+
         ul {
             margin: 12px 0 12px 24px;
             list-style: disc;
@@ -63,6 +115,28 @@
             color: #e26f0f;
             text-decoration: underline;
         }
+    }
+
+    .text__requisites {
+        padding: 10px;
+        background: rgba(255, 255, 255, 0.1);
+        border-radius: 10px;
+        font-size: 1rem;
+
+        p {
+            display: flex;
+            align-items: center;
+
+            i {
+                margin-left: 8px;
+                cursor: pointer;
+                color: #e26f0f;
+            }
+        }
+
+    }
+    img {
+        margin: 0 auto;
     }
 }
 </style>
