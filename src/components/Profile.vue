42f3dc417aa678144865bfef0b26554fb5f3b867
R-omek <rmngrck97@gmail.com>
2025-09-18 11:46:14 +0300

    add png and jpeg filter

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -140,7 +140,7 @@ const show = () => {
                                         <path d="M11 0C4.9 0 0 4.9 0 11C0 17.1 4.9 22 11 22C17.1 22 22 17.1 22 11C22 4.9 17.1 0 11 0ZM16 13H13V16C13 17.1 12.1 18 11 18C9.9 18 9 17.1 9 16V13H6C4.9 13 4 12.1 4 11C4 9.9 4.9 9 6 9H9V6C9 4.9 9.9 4 11 4C12.1 4 13 4.9 13 6V9H16C17.1 9 18 9.9 18 11C18 12.1 17.1 13 16 13Z" fill="#E26F0F"/>
                                     </svg>
                                 </label>
-                                <input @change="changeAvatar" id="addAvatarInput" class="hidden" type="file">
+                                <input @change="changeAvatar" id="addAvatarInput" class="hidden" type="file" accept="image/png, image/jpeg">
                             </div>
                         </div>
                     </div>
