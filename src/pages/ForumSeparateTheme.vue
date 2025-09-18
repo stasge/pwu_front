8a4d0e1d9144506535078bb20fe28cca5adb60cf
R-omek <rmngrck97@gmail.com>
2025-09-18 11:03:25 +0300

    style: add word-break property to article class for better text handling

--- src/pages/ForumSeparateTheme.vue
+++ src/pages/ForumSeparateTheme.vue
@@ -434,6 +434,7 @@ function onCloseCommentPicker() {
 <style scoped lang='scss'>
 .article {
     color: #FFF;
+    word-break: break-word;
 
     &__title {
         text-shadow: 3px 0px 7px rgba(81, 67, 21, 0.8), -3px 0px 7px rgba(81, 67, 21, 0.8), 0px 4px 7px rgba(81, 67, 21, 0.8);
